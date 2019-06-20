d01f51e6b56211502be8ba41f0e3a5fa62773c48
Mike Roberts <mikebroberts@hotmail.com>
2019-06-20 01:02:13 -0600

    set up ejs views, pulled in bootstrap template and did some styling on home page

--- app.js
+++ app.js
@@ -4,15 +4,25 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 3000
+
+// EJS
+app.set('view engine', 'ejs');
+
+// Use Body Parser
+app.use(bodyParser.urlencoded({ extended: true}));
+
+// allow static pages to be accessed
+app.use(express.static("public"));
+
 app.get("/", (req,res)=>{
-    res.sendFile(__dirname +"/index.html");
+    res.render('home');
 });
 
-app.listen(PORT, (err)=>{
-    if(err){
-       console.error(err)
-    }else{
-        console.log("Server started on port " + PORT);
-    }    
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, ()=>{ 
+    console.log("Server started on port " + port); 
 });
